7ec44f46f3b9f134bf4443100c54ae4f850e6777
Midori Takahashi <mintgreen7programming@gmail.com>
2024-02-04 15:05:08 +0900

    chatInput

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import './Chat.scss'
 import ChatHeader from './ChatHeader'
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import GifIcon from '@mui/icons-material/Gif';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 
 function Chat() {
   return (
     <div className='chat'>
       <ChatHeader />
       <div className='chatMessage'>Chat</div>
-      <div className='chatInput'>Chat</div>
+      <div className='chatInput'>
+        <AddCircleOutlineIcon />
+        <form>
+          <input type='text' placeholder='メッセージを送信'/>
+          <button type='submit' className='chatInputButton'>送信</button>
+        </form>
+        <div className='chatInputIcons'>
+          <CardGiftcardIcon />
+          <GifIcon />
+          <EmojiEmotionsIcon />
+        </div>
+      </div>
     </div>
   )
 }
