2710a83871a4a6d2431e0ced189c306fc3febe65
Midori Takahashi <mintgreen7programming@gmail.com>
2024-02-10 06:45:11 +0900

    add login

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,26 @@ import React from 'react';
 import "./App.scss";
 import Sidebar from "./components/sidebar/Sidebar";
 import Chat from './components/chat/Chat';
+import { useSelector } from 'react-redux';
+import Login from './components/login/Login';
+import { useAppSelector } from './app/hooks';
 
 function App() {
+
+  const user = useAppSelector((state) => state.user)
+
   return (
     <div className="App">
-      <Sidebar />
-      <Chat />
+      {user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ) : (
+        <>
+        <Login />
+        </>
+      )}
     </div>
   );
 }
