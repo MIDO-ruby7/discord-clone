e84df2415dd6491c8fc2670b9c6ecf22ddb036f4
Midori Takahashi <mintgreen7programming@gmail.com>
2024-02-10 06:45:29 +0900

    add hooks

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,4 +3,7 @@ import useReducer  from "../features/userSlice";
 
 export const store = configureStore({
   reducer: useReducer,
-})
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
